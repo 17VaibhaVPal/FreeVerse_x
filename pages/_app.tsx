ad8140c5b1cd43c368268c2b98d269805949c3de
17VaibhaVPal <vaibhavpal17sept@gmail.com>
2025-06-20 02:26:56 +0530

    Fix: remove duplicate global CSS import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import { Inter } from "next/font/google";
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import {Toaster} from 'react-hot-toast'
 import {QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
-const queryClient = new QueryClient()
 
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
