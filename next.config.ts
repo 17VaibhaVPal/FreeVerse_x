43ccbc735712beaeb5d585ae6674df874fbf963e
17VaibhaVPal <vaibhavpal17sept@gmail.com>
2025-06-20 02:13:26 +0530

    disable eslint blocking on build

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
       "vaibhav-twitter-dev.s3.ap-south-1.amazonaws.com",
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Add this line to skip lint errors in production build
+  },
 };
 
 export default nextConfig;
